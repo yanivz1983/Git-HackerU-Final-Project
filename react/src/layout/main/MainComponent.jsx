3a919fce78dc18c901f37b0067af66dae99cdee0
yanivz1983 <139677277+yanivz1983@users.noreply.github.com>
2024-03-09 12:31:35 -0500

    final 6

--- react/src/layout/main/MainComponent.jsx
+++ react/src/layout/main/MainComponent.jsx
@@ -2,9 +2,7 @@ import { Container } from "@mui/material";
 
 const MainComponent = ({ children }) => {
   return (
-    <Container sx={{ mb: "6%", mt: 20, minHeight: "65vh" }}>
-      {children}
-    </Container>
+    <Container sx={{ mb: 15, mt: 20, minHeight: "65vh" }}>{children}</Container>
   );
 };
 
